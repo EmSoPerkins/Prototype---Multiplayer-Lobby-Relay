fd82dbb0609f64e81c244cd77fd0e0e98860cf49
John Perkins <147350666+EmSoPerkins@users.noreply.github.com>
2023-10-08 21:03:24 -0400

    Basic Multiplayer, Imports
    Basic Multiplayer Functionality Added
    Clients can join a host
    Added Parrel Sync for Multiplayer Testing Multiple Clients
    Added Lobby and Game Scenes to Project Build Settings

--- Assets/Scripts/InnGameMultiplayer.cs
+++ Assets/Scripts/InnGameMultiplayer.cs
@@ -3,9 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InnGameMultiplayer : MonoBehaviour
 {
+    private const string GAMEPLAY_SCENE = "GameScene";
+    private const string LOBBY_SCENE = "LobbyScene";
+
     public static InnGameMultiplayer Instance { get; private set; }
 
     private void Awake()
@@ -20,6 +24,7 @@ public class InnGameMultiplayer : MonoBehaviour
     { 
         Debug.Log("Host Started"); 
         NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(LOBBY_SCENE, LoadSceneMode.Single);
     }
 
     public void StartClient()
